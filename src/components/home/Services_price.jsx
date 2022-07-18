ce286a2c408ab4835e07440d5641dac15aeb80e2
lamnguyen03 <97352277+lamnguyen03@users.noreply.github.com>
2022-07-18 22:39:09 +0700

    Lam push structure of homepage

--- src/components/home/Services_price.jsx
+++ src/components/home/Services_price.jsx
@@ -4,51 +4,183 @@ import $ from "jquery";
 
 const Services = () => {
   return (
-    <div className="container">
-      <div className="left-content">
-        <h1>Spa May</h1>
-        <h5>Bảng giá dịch vụ </h5>
-        <div className="combo">
-          <img
-            src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/07/Procedure-11-150x150.jpg"
-            alt=""
-          />
-          <p>
-            Hot Stone Massage <br />
-            20 phút Phục hồi da mặt
-          </p>
-          <p>$30.00</p>
+    <div>
+      <div className="container">
+        <div className="left-content">
+          <div className="name-title">
+            <p className="spa">Spa May</p>
+            <p className="head">Bảng giá dịch vụ </p>
+          </div>
+          <div className="combo">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/07/Procedure-11-150x150.jpg"
+              alt=""
+            />
+            <p className="ser-name">
+              Hot Stone Massage <br />
+              20 phút Phục hồi da mặt
+            </p>
+            <p>$30.00</p>
+          </div>
+          <br />
+          <div className="combo">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/07/Procedure-15-150x150.jpg"
+              alt=""
+            />
+            <p className="ser-name">
+              Hot Stone Massage <br />
+              20 phút Phục hồi da mặt
+            </p>
+            <p>$30.00</p>
+          </div>
+          <br />
+          <div className="combo">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/07/Procedure-5-150x150.jpg"
+              alt=""
+            />
+            <p className="ser-name">
+              Oriental Spa Manicure <br />
+              20 phút Phục hồi da mặt
+            </p>
+            <p>$30.00</p>
+          </div>
         </div>
-        <br />
-        <div className="combo">
-          <img
-            src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/07/Procedure-11-150x150.jpg"
-            alt=""
-          />
-          <p>
-            Hot Stone Massage <br />
-            20 phút Phục hồi da mặt
-          </p>
-          <p>$30.00</p>
-        </div>
-        <br />
-        <div className="combo">
-          <img
-            src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/07/Procedure-11-150x150.jpg"
-            alt=""
-          />
-          <p>
-            Hot Stone Massage <br />
-            20 phút Phục hồi da mặt
-          </p>
-          <p>$30.00</p>
+        <div className="right-content">
+          <div className="cont">
+            <div className="name-title">
+              <p className="spa">Spa May</p>
+              <p className="head">Hãy để chúng tôi phục vụ bạn</p>
+            </div>
+            <button className="btn">Đặt dịch vụ </button>
+          </div>
         </div>
       </div>
-      <div className="right-content">
-        <div className="cont">
-          <h1>Spa May</h1>
-          <h1>Hãy để chúng tôi phục vụ bạn</h1>
-          <button className="btn">Đặt dịch vụ </button>
+      <div className="all_service">
+        <b>
+          Hãy cho bản thân bạn, và làm trẻ hoá làn tâm trí & tâm hồn của bạn
+        </b>
+        <hr />
+        <p>Nơi thiên nhiên gặp hạnh phúc</p>
+        <button>Khám phá tất cả dịch vụ</button>
+      </div>
+      <div className="outstanding">
+        <div className="name-title">
+          <p className="spa">Spa May</p>
+          <p className="head">Dịch vụ nổi bật</p>
+        </div>
+        <div className="item">
+          <div className="item1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/img1-150x150.jpg"
+              alt=""
+            />
+            <p>1. Massage Therapy</p>
+            <p>
+              Chúng tôi hiểu rằng thư giãn thực sự là một hình thức nghệ thuật.
+              Kết hợp một bài massage phù hợp với dịch vụ Chăm Sóc Da nói chung
+              và Da Mặt nói riêng, hoặc các dịch vụ thẩm mỹ khác
+            </p>
+            <button>Xem ngay </button>
+          </div>
+          <div className="item1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/img24-150x150.jpg"
+              alt=""
+            />
+            <p>2. Skin Care</p>
+            <p>
+              Chúng tôi hiểu rằng thư giãn thực sự là một hình thức nghệ thuật.
+              Kết hợp một bài massage phù hợp với dịch vụ Chăm Sóc Da nói chung
+              và Da Mặt nói riêng, hoặc các dịch vụ thẩm mỹ khác
+            </p>
+            <button>Xem ngay </button>
+          </div>
+          <div className="item1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/img25-150x150.jpg"
+              alt=""
+            />
+            <p>3. Body Treatments</p>
+            <p>
+              Chúng tôi hiểu rằng thư giãn thực sự là một hình thức nghệ thuật.
+              Kết hợp một bài massage phù hợp với dịch vụ Chăm Sóc Da nói chung
+              và Da Mặt nói riêng, hoặc các dịch vụ thẩm mỹ khác
+            </p>
+            <button>Xem ngay </button>
+          </div>
+        </div>
+      </div>
+      <div className="reason">
+        <div className="name-title">
+          <p className="spa">Spa May</p>
+          <p className="head">HÃY ĐẾN VỚI SPA MÂY</p>
+        </div>
+        <div className="re">
+          <div className="re1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/ic1.png"
+              alt=""
+            />
+            <p>Break from Routine</p>
+            <hr />
+            <p>
+              Cách tốt nhất để thư giãn hoàn toàn và giảm căng thẳng cho chính
+              mình.
+            </p>
+          </div>
+          <div className="re1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/ic6.png"
+              alt=""
+            />
+            <p>Improves Blood Circulation</p>
+            <hr />
+            <p>Cung cấp thêm oxy & chất dinh dưỡng cho các tế bào.</p>
+          </div>
+          <div className="re1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/ic8.png"
+              alt=""
+            />
+            <p>Manage Chronic Conditions</p>
+            <hr />
+            <p>
+              Liệu pháp xoa bóp mang lại lợi ích vượt trội cho việc kiểm soát
+              cơn đau.
+            </p>
+          </div>
+          <div className="re1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/ic9.png"
+              alt=""
+            />
+            <p>Body Mind & Soul</p>
+            <hr />
+            <p>
+              Spa cải thiện cảm giác tốt và mang lại sự hài hòa cho cơ thể, tâm
+              trí và tâm hồn.
+            </p>
+          </div>
+          <div className="re1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/ic7.png"
+              alt=""
+            />
+            <p>Deep Tissue Massage</p>
+            <hr />
+            <p>Chuyên gia trị liệu làm giảm căng thẳng cơ bắp và chuột rút.</p>
+          </div>
+          <div className="re1">
+            <img
+              src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/ic10.png"
+              alt=""
+            />
+            <p>Keeping Fit & Healthy</p>
+            <hr />
+            <p>Spa giảm cân thêm để giữ cho cơ thể của bạn trong hình dạng.</p>
+          </div>
         </div>
       </div>
     </div>
