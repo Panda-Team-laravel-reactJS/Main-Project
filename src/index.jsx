136c8b757995e714b3dbbef7419a082bf3b03697
lamnguyen03 <97352277+lamnguyen03@users.noreply.github.com>
2022-07-22 21:08:08 +0700

    merge dev

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./layouts";
 import "./assets/scss/index.scss";
-import { AboutUs, ExamplePage, HomePage, ServicesPage } from "./pages";
-
+import { ServicesPrice } from "./components/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<MainLayout />}>
-				{/* <Route path="/" element={<HomePage />}/> */}
-				<Route path="AboutUs" element={<AboutUs/>} />
-				<Route path="Example" element={<ExamplePage />} />
-				<Route path="Services" element={<ServicesPage />} />
-			</Route>
-		</Routes>
-	</BrowserRouter>
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="Services" element={<ServicesPrice />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
