b6f2dd9b1f029393e2d512c16106d102594efc95
NDK2509 <Dangkyqt2509@gmail.com>
2022-07-24 08:07:32 +0700

    Add HomePage route

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./layouts";
 import "./assets/scss/index.scss";
-import { ServicesPrice } from "./components/home";
+import HomePage from "./pages/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path="Services" element={<ServicesPrice />} />
+        <Route path="" element={<HomePage />} />
       </Route>
     </Routes>
   </BrowserRouter>
