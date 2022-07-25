eba298149ede29dc6026253eac6e8f4ea3b4b3fd
lamnguyen03 <97352277+lamnguyen03@users.noreply.github.com>
2022-07-25 22:06:23 +0700

    Re-separate component of homepage

---
+++ src/components/home/OutStanding.jsx
@@ -0,0 +1,52 @@
+const Outstanding = () => {
+  return (
+    <div className="outstanding">
+      <div className="name-title">
+        <p className="spa">Spa May</p>
+        <p className="head">Dịch vụ nổi bật</p>
+      </div>
+      <div className="item">
+        <div className="item1">
+          <img
+            src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/img1-150x150.jpg"
+            alt=""
+          />
+          <p>1. Massage Therapy</p>
+          <p>
+            Chúng tôi hiểu rằng thư giãn thực sự là một hình thức nghệ thuật.
+            Kết hợp một bài massage phù hợp với dịch vụ Chăm Sóc Da nói chung và
+            Da Mặt nói riêng, hoặc các dịch vụ thẩm mỹ khác
+          </p>
+          <button>Xem ngay </button>
+        </div>
+        <div className="item1">
+          <img
+            src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/img24-150x150.jpg"
+            alt=""
+          />
+          <p>2. Skin Care</p>
+          <p>
+            Chúng tôi hiểu rằng thư giãn thực sự là một hình thức nghệ thuật.
+            Kết hợp một bài massage phù hợp với dịch vụ Chăm Sóc Da nói chung và
+            Da Mặt nói riêng, hoặc các dịch vụ thẩm mỹ khác
+          </p>
+          <button>Xem ngay </button>
+        </div>
+        <div className="item1">
+          <img
+            src="http://mauweb.monamedia.net/helenspa/wp-content/uploads/2019/06/img25-150x150.jpg"
+            alt=""
+          />
+          <p>3. Body Treatments</p>
+          <p>
+            Chúng tôi hiểu rằng thư giãn thực sự là một hình thức nghệ thuật.
+            Kết hợp một bài massage phù hợp với dịch vụ Chăm Sóc Da nói chung và
+            Da Mặt nói riêng, hoặc các dịch vụ thẩm mỹ khác
+          </p>
+          <button>Xem ngay </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Outstanding;
